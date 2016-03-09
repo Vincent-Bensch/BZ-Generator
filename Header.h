8f8079e4c34df326180d7169932e29239c2f1eb8
Vincent Bensch <vbensch@gmail.com>
2016-03-09 15:07:36 -0600

    Made changes to variable names in vmath classes for easier reading and fixed some errors introduced by brackets accidentialy deleted in the last commit.

--- Header.h
+++ Header.h
@@ -24,7 +24,7 @@ namespace vmath { //To avoid name conflicts, I put all my custom classes and var
 	class timer {
 	public:
 		double a; //Using doubles as my numeric variable type in the entire program for precision consistancy
-		double b; 
+		double b;
 		void reset() { a = clock(); return; } //Restart stores current clock time
 		double read() { return (clock() - a) / (double)CLOCKS_PER_SEC; } //Return ticks passed since last reset command and devide by ticks per second
 	};
@@ -38,92 +38,92 @@ namespace vmath { //To avoid name conflicts, I put all my custom classes and var
 		double magnitude() { return sqrt(pow(i, 2) + pow(j, 2) + pow(k, 2)); } //Pythagrian theorem to get vector magnitude
 
 		vector normalize() { //Return a unit vector pointing the same direction as orignial vector, by deviding all components by magnitude
-			vector o;
+			vector out;
 			double l = magnitude();
-			o.populate(
+			out.populate(
 				i / l,
 				j / l,
 				k / l);
-			return o;
+			return out;
 		}
 
-		double dot(vector v) { return i*v.i + j*v.j + k*v.k; } //a.dot(b) returns the dot product of a with b
+		double dot(vector in) { return i*in.i + j*in.j + k*in.k; } //a.dot(b) returns the dot product of a with b
 
-		vector cross(vector v) { // a.cross(b) returnes the cross poduct of a with b
-			vector o;
-			o.populate(
-				j*v.k - k*v.j,
-				k*v.i - i*v.k,
-				i*v.j - j*v.i);
-			return o;
+		vector cross(vector in) { // a.cross(b) returnes the cross poduct of a with b
+			vector out;
+			out.populate(
+				j*in.k - k*in.j,
+				k*in.i - i*in.k,
+				i*in.j - j*in.i);
+			return out;
 		}
 
-		vector add(vector v) { //a.add(b) returns vector a plus vector b
-			vector o;
-			o.populate(
-				i + v.i,
-				j + v.j,
-				k + v.k);
-			return o;
+		vector add(vector in) { //a.add(b) returns vector a plus vector b
+			vector out;
+			out.populate(
+				i + in.i,
+				j + in.j,
+				k + in.k);
+			return out;
 		}
 
-		vector subtract(vector v) { //a.subtract(b) returns vector a minus vector b
-			vector o;
-			o.populate(
-				i - v.i,
-				j - v.j,
-				k - v.k);
-			return o;
+		vector subtract(vector in) { //a.subtract(b) returns vector a minus vector b
+			vector out;
+			out.populate(
+				i - in.i,
+				j - in.j,
+				k - in.k);
+			return out;
 		}
 
 		vector multiply(double d) { //a.multiply(b) returns vector a with all elements multiplied by double b
-			vector o;
-			o.populate(
+			vector out;
+			out.populate(
 				i*d,
 				j*d,
 				k*d);
-			return o;
+			return out;
 		}
 
 		vector divide(double d) { //a.devide(b) returns vector a will all elements devided by double b
-			vector o;
-			o.populate(
+			vector out;
+			out.populate(
 				i / d,
 				j / d,
 				k / d);
-			return o;
+			return out;
 		}
 	};
 
 	class line { //Class for an enless line definded by a position and a direction vector
 	public:
-		vector direction; //Vector denoting direction of the line
-		vector location; //Position vector marking a point on the line
+		vector dir; //Vector denoting direction of the line
+		vector loc; //Position vector marking a point on the line
 
 	};
 
 	class plane { //Class for a plane defined by a position and a normal vector
 	public:
-		vector location; //Position vector marking a point on the plane
-		vector normal; //Vector denoting the normal direction of the plane
+		vector norm; //Vector denoting the normal direction of the plane
+		vector loc; //Position vector marking a point on the plane
 
-		}
 	};
 
 	class linesegment { //Class for a linesegment defined by a start and an end point
 	public:
-		vector start, end; //Position vectors for the start and end points
+		vector a, b; //Position vectors for the start and end points
 
 		vector midpoint() { //Retrun a position vector marking the midpoint of the linesegment
-			vector o;
-			o.populate(
-				(start.i + end.i) / 2,
-				(start.j + end.j) / 2,
-				(start.k + end.k) / 2);
-			return o;
+			vector out;
+			out.populate(
+				(a.i + b.i) / 2,
+				(a.j + b.j) / 2,
+				(a.k + b.k) / 2);
+			return out;
 		}
 
-		double length() { return sqrt(pow(end.i - start.i, 2) + pow(end.j - start.j, 2) + pow(end.k - start.k, 2)); } //Pythagrian theorem for the length of the linesegment
+		double length() { return sqrt(pow(b.i - a.i, 2) + pow(b.j - a.j, 2) + pow(b.k - a.k, 2)); } //Pythagrian theorem for the length of the linesegment
+	};
 }
 
 #endif
