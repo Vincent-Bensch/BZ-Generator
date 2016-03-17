0a419c15847bd6a1561eec5d59851e7270035f0b
Vincent Bensch <vbensch@gmail.com>
2016-03-17 14:00:14 -0500

    Added debug output code to UI

--- Source.cpp
+++ Source.cpp
@@ -18,19 +18,27 @@ void ui() {
 	double latticestep = requestdouble("Step for Lattice Points");
 	int maximumzone = requestint("Highest zone you care about");
 	outloc = requeststring("Base filename for file output");
-	system("CLS");
-
+	cout << endl << endl;
 	report("Parameters received \n			Lattice Type: " + latticetype 
 		+ "\n			Maximum distance from origin for Lattice Points: " + str(maxlatticedistance) 
 		+ "\n			Step for Lattice Points: " + str(latticestep)
 		+ "\n			Highest zone you car about: " + str(maximumzone)
 		+ "\n			Base filename for file output: " + outloc);
+
 	if (latticetype == "PC") { uilatticepoints = makePClattice(maxlatticedistance, latticestep); }
 	if (latticetype == "FCC") { uilatticepoints = makeFCClattice(maxlatticedistance, latticestep); }
 	if (latticetype == "BCC") { uilatticepoints = makeBCClattice(maxlatticedistance, latticestep); }
+	report("Lattice points generated");
 
 	uilinesegmentlist = makelinesegmentsfromlattice(uilatticepoints);
+	report("Line segments generated from lattice");
+
 	uiplanelist = makebisectorplanes(uilinesegmentlist);
+	report("Bisector planes generated");
+
 	uilinesbyplanes = makelinesfromplanes(uiplanelist);
+	report("Lines generated on planes");
+
 	linesintopolygons(uilinesbyplanes, uiplanelist, maximumzone);
+	report("Done");
 }
